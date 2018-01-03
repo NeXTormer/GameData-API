6f10c3c03a387ae2545d71d3c0f1cbc92f9016d7
Felix Holz <felix.holz@me.com>
2018-01-03 14:53:52 +0100

    fixed bug where players are inserted multiple times

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 console.log("Server is starting...");
 
 var express = require("express");
+
 var app = express();
 
 var mysql = require("mysql");
 var connection = mysql.createConnection({
-    host: "faoiltiarna.ddns.net",
+    host: "10.0.0.254",
+    port: "3306",
     user: "user",
     password: "PeterRendl69!",
     database: "gamedata"
@@ -105,9 +107,34 @@ function addScore(request, response)
     }
 
 
-    connection.query("SELECT id FROM players WHERE name = \"" + player + "\";",
+    var query = "SELECT id FROM players WHERE name = \"" + player + "\";";
+    connection.query(query,
         function(err, resp, fields) {
-            if(!resp.id)
+        var t_id;
+        if(resp instanceof Array)
+        {
+            try
+            {
+                t_id = resp[0].id;
+            }
+            catch(e)
+            {
+                t_id = -1;
+            }
+        }
+        else
+        {
+            try
+            {
+                t_id = resp.id;
+            }
+            catch(e)
+            {
+                t_id = -1;
+            }
+        }
+
+            if(t_id === -1)
             {
                 connection.query("INSERT INTO players (name, regdate) VALUES (\"" + player + "\", now());",
                     function(err, res) {
@@ -121,8 +148,12 @@ function addScore(request, response)
 
                                     connection.query("INSERT INTO scores (score, player_id, game_id, date) VALUES (?, ?, ?, now());", [score, playerid, gameid],
                                         function(err, resp) {
-                                            response.send(err);
-                                        })
+                                            var message = "Created new player [ " + playerid + ", " + player + " ] and inserted the score [ " + score + " ] for the game [ " + gameid + ", " + game + " ].";
+                                            console.log(message);
+                                            response.send({
+                                                msg: message
+                                            });
+                                        });
                                 }
 
                             });
@@ -132,26 +163,54 @@ function addScore(request, response)
             }
             else
             {
-                var playerid = resp[0].id;
-
-                connection.query("SELECT id FROM games WHERE name = \"" + game + "\";",
+                console.log(t_id);
+                var e_query = "SELECT id FROM games WHERE name = \"" + game + "\";";
+                connection.query(e_query,
                     function(err, resp, fields) {
-                        if(resp.id)
-                        {
-                            var gameid = resp.id;
 
-                            connection.query("INSERT INTO scores (score, player_id, game_id, date) VALUES (?, ?, ?, now());", [score, playerid, gameid],
+                        var g_id;
+                        if(resp instanceof Array)
+                        {
+                            try
+                            {
+                                g_id = resp[0].id;
+                            }
+                            catch(e)
+                            {
+                                g_id = -1;
+                            }
+                        }
+                        else
+                        {
+                            try
+                            {
+                                g_id = resp.id;
+                            }
+                            catch(e)
+                            {
+                                g_id = -1;
+                            }
+                        }
+
+                        if(g_id !== -1)
+                        {
+                            var query2 = "INSERT INTO scores (score, player_id, game_id, date) VALUES (\"" + score + "\", \"" + t_id + "\", \"" + g_id + "\", now());";
+                            connection.query(query2,
                                 function(err, resp) {
-                                    if(!err)
-                                    {
-                                        response.send("Inserted score.")
-                                    }
-                                })
+                                    var message = "requested existing player [ " + t_id + ", " + player + " ] and inserted the score [ " + score + " ] for the game [ " + g_id + ", " + game + " ]."
+                                    console.log(message);
+                                    response.send({msg: message});
+                                });
+                        }
+                        else
+                        {
+                            message = "could not find game " + game;
+                            console.log(message);
+                            response.send({msg: message});
                         }
 
                     });
             }
-
         });
 }
 
